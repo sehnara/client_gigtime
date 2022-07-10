78e3aa99bb989343f30e49ca08ac95cd7408c614
sehnara <gsh723@naver.com>
2022-07-10 20:32:06 +0900

    for setting

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Hello from "./pages/hello";
+
+function App() {
+  //GET TEST
+  const getTest = async () => {
+    const response = await axios("http://localhost:8080/tweets").then((res) => {
+      const data = res.data;
+      const a = data.name;
+      console.log(a);
+    });
+    return response;
+  };
+
+  useEffect(() => {
+    getTest();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        <Route path="/hello" element={<Hello />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/hello.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hello = () => {
+  return <div>hihiihihhihihihih</div>;
+};
+
+export default Hello;
