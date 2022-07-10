457f2d74096bf77f8170519e2adcbf99e6c090c0
sehnara <gsh723@naver.com>
2022-07-10 20:59:04 +0900

    for setting

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="bg-red dark:bg-gray-800">
+    <div className="text-red-500">
       <p>hello</p>
     </div>
   );
