f1668e11ea889ff0188b6bcc4128d4118277b5f2
sehnara <gsh723@naver.com>
2022-07-17 14:53:37 +0900

    !!

--- src/components/InterviewCard.tsx
+++ src/components/InterviewCard.tsx
@@ -15,47 +15,42 @@ type InterviewCardProps = {
   storeType: string[];
 };
 
-const InterviewCard = ({ date, time, rejectFlag, resultFlag, link, state, storeName, storeAdd, storeType} : InterviewCardProps) => {
-  const navigate = useNavigate()
-  let dateSplit: string[] = date.split('-');
+const InterviewCard = ({
+  date,
+  time,
+  rejectFlag,
+  resultFlag,
+  link,
+  state,
+  storeName,
+  storeAdd,
+  storeType,
+}: InterviewCardProps) => {
+  const navigate = useNavigate();
+  let dateSplit: string[] = date.split("-");
   const interviewTime = `${time}:00 ~ ${time + 1}:00`;
-  
+
   function linkClick() {
     navigate(`${link}`);
   }
-
-  // const [nowState, setState] = useState('');
-  // if (state === 1) {
-  //   setState('입장대기');
-  // } else if (state === 2) {
-  //   setState('수락대기');
-  // } else if (state === 3) {
-  //   if (rejectFlag === 1) {
-  //     setState("거절");
-  //   } else {
-  //     setState('수락');
-  //   }
-  // } else {
-  //   if (resultFlag === 1) {
-  //     setState('합격');
-  //   } else {
-  //     setState('불합격');
-  //   }
-  // }
-
-  return(
+  
+  return (
     <div className="mx-8 my-4 h-10 mb-32">
       <div className="flex items-center w-full my-4">
         <AiOutlineCalendar className="mr-2" />
-        <p className="text-xs">{dateSplit[0]}년 {dateSplit[1]}월 {dateSplit[2]}일</p>
+        <p className="text-xs">
+          {dateSplit[0]}년 {dateSplit[1]}월 {dateSplit[2]}일
+        </p>
       </div>
       <div className="flex ">
         {/* 가게명 */}
         <h3 className="font-bold text-base">{storeName}</h3>
         {/* 직종 */}
-        {storeType.map((el, index) => 
-          <p className="text-xs ml-6 bg-gray-200 px-2 py-1 rounded-2xl">{el[index]}</p>
-        )}
+        {storeType.map((el, index) => (
+          <p className="text-xs ml-6 bg-gray-200 px-2 py-1 rounded-2xl">
+            {el[index]}
+          </p>
+        ))}
       </div>
       {/* 주소 */}
       <p className="mb-3 text-gray-500 text-sm mt-1">{storeAdd}</p>
@@ -69,6 +64,6 @@ const InterviewCard = ({ date, time, rejectFlag, resultFlag, link, state, storeN
       </div>
     </div>
   );
-}
+};
 
 export default InterviewCard;
--- src/pages/InWorkerMyPages/InterviewTimeTable.jsx
+++ src/pages/InWorkerMyPages/InterviewTimeTable.jsx
@@ -3,20 +3,20 @@ import InterviewCard from "../../components/InterviewCard";
 
 const InterviewTimeTable = ({ result }) => {
   const src = result;
-  
+
   return (
     <div className="overflow-scroll h-96">
-      {src.map(el => (
-        <InterviewCard 
-        date={el.interview_date}
-        time={el.interview_time}
-        rejectFlag={el.reject_flag}
-        resultFlag={el.result_flag}
-        link={el.link}
-        state={el.state}
-        storeName={el.store_name}
-        storeAdd={el.store_address}
-        storeType={el.store_type}
+      {src.map((el) => (
+        <InterviewCard
+          date={el.interview_date}
+          time={el.interview_time}
+          rejectFlag={el.reject_flag}
+          resultFlag={el.result_flag}
+          link={el.link}
+          state={el.state}
+          storeName={el.store_name}
+          storeAdd={el.store_address}
+          storeType={el.store_type}
         />
       ))}
     </div>
