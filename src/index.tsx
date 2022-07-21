672bf67c47913ebed936bbe5bdbbd1a24385828e
yeopto <gunyeop530@gmail.com>
2022-07-22 02:20:42 +0900

    [ADD] PWA Lighthouse 검사 통과

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,17 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./module/store";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
-);
-root.render(
-  <Provider store={store}>
+  );
+  root.render(
+    <Provider store={store}>
     <App />
   </Provider>
 );
+
+serviceWorkerRegistration.register();
+reportWebVitals();
