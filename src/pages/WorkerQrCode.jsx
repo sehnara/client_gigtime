4e9978b43b5f8bb586cdfaf17127237986960a5b
sehnara <gsh723@naver.com>
2022-07-26 00:07:25 +0900

    [ADD] 알바생 QR코드 생성 페이지 완료

--- src/pages/WorkerQrCode.jsx
+++ src/pages/WorkerQrCode.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import QRCode from "react-qr-code";
+import Header from "../components/Header";
 const WorkerQrCode = () => {
-  return <div></div>;
+
+  
+
+  return (
+    <div>
+      <Header title="출근하기" />
+      <div className="flex justify-center items-center p-8 py-12 bg-cyan-500">
+        <QRCode
+          value={`
+            worker_id: ${sessionStorage.getItem("worker_id")},
+            time: ${new Date()}`}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default WorkerQrCode;
