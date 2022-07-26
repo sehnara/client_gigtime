8e42cd096437c14bdda3db55c7868cbc97c60b2d
sehnara <gsh723@naver.com>
2022-07-27 03:56:00 +0900

    [MERGE] merge conflict

--- src/pages/OwnerRecruitNoticePage.jsx
+++ src/pages/OwnerRecruitNoticePage.jsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from '../components/Button';
-import Header from '../components/Header';
-import InputValue2 from '../components/InputValue2';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import Header from "../components/Header";
+import InputValue2 from "../components/InputValue2";
 
 // {
 //   'owner_id': 60,
@@ -55,149 +55,163 @@ const OwnerRecruitNoticePage = () => {
 
   const getData = async () => {
     await axios
-      .post(`${process.env.REACT_APP_ROUTE_PATH}/owner/mypage/employment/button`, {
-        owner_id: owner_id,
-        store_name: '',
-        type: '',
-        description: '',
-        start_date: '',
-        end_date: '',
-        start_time: '',
-        end_time: '',
-        price: 0,
+      .post(
+        `${process.env.REACT_APP_ROUTE_PATH}/owner/mypage/employment/button`,
+        {
+          owner_id: owner_id,
+        }
+      )
+      .then((res) => {
+        console.log("recruit", res.data);
+        setRecruitData({
+          ...recruitData,
+          store_name: res.data.name,
+          address: res.data.address,
+          types: res.data.types,
+        });
+      });
+  };
+
+  const onEnroll = async () => {
+    await axios
+      .post(`${process.env.REACT_APP_ROUTE_PATH}/owner/employment`, postData)
+      .then((res) => {
+        navigate("/owner/mypage");
+        console.log(res.data);
+      });
+  };
+
+  useEffect(() => {
+    setPostData({
+      ...postData,
+      store_name: recruitData.store_name,
+      type: recruitData.types,
+      description: recruitData.description,
+      start_date: recruitData.start_date,
+      end_date: recruitData.end_date,
+      start_time: recruitData.start_time,
+      end_time: recruitData.end_time,
+      price: recruitData.price,
     });
-    const setValue = (_key, _value) => {
-        setRecruitData({ ...recruitData, [_key]: _value });
-    };
+  }, [recruitData]);
 
-    const onEnroll = async () => {
-        await axios.post(`${process.env.REACT_APP_ROUTE_PATH}/owner/employment`, postData).then((res) => {
-            navigate('/owner/mypage');
-            console.log(res.data);
-        });
-    };
+  useEffect(() => {
+    getData();
+  }, []);
 
-    useEffect(() => {
-        setPostData({
-            ...postData,
-            store_name: recruitData.store_name,
-            type: recruitData.types,
-            description: recruitData.description,
-            start_date: recruitData.start_date,
-            end_date: recruitData.end_date,
-            start_time: recruitData.start_time,
-            end_time: recruitData.end_time,
-            price: recruitData.price,
-        });
-    }, [recruitData]);
+  return (
+    <div className="">
+      <Header title={"모집공고"} />
+      <div className="mx-8">
+        {/* 매장이름 */}
+        <InputValue2
+          label={"매장이름"}
+          setValue={setValue}
+          dict_key={"store_name"}
+          dict_value={recruitData["store_name"]}
+        />
+        {/* 매장주소 */}
+        <InputValue2
+          label={"매장주소"}
+          setValue={setValue}
+          dict_key={"address"}
+          dict_value={recruitData["address"]}
+        />
+        {/* 알바유형 */}
+        <InputValue2
+          mode="SELECT"
+          label={"알바유형"}
+          title={"알바유형 선택"}
+          setValue={setValue}
+          dict_key={"type"}
+          dict_value={recruitData["types"]}
+          options={recruitData["types"]}
+        />
+        {/* 상세설명 */}
+        <InputValue2
+          title="해당 공고에 대한 설명 부탁드려요."
+          label={"상세설명"}
+          setValue={setValue}
+          dict_key={"description"}
+          dict_value={recruitData["description"]}
+        />
+      </div>
+      <div className="border-t-4 mt-8 mb-4"></div>
+      {/* --------------------------------------------- */}
 
-    useEffect(() => {
-        getData();
-    }, []);
+      <div className="mx-8">
+        {/* 시작날짜 */}
+        <InputValue2
+          mode="DATE"
+          label={"시작날짜"}
+          title={"시작 날짜 선택"}
+          setValue={setValue}
+          dict_key={"start_date"}
+          dict_value={recruitData["start_date"]}
+        />
+        {/* 종료날짜 */}
+        <InputValue2
+          mode="DATE"
+          label={"종료날짜"}
+          title={"종료 날짜 선택"}
+          setValue={setValue}
+          dict_key={"end_date"}
+          dict_value={recruitData["end_date"]}
+        />
+        {/* 근무 시간 */}
+        <InputValue2
+          mode="TIME"
+          label="근무시간"
+          options={[
+            "10:00",
+            "11:00",
+            "12:00",
+            "13:00",
+            "14:00",
+            "15:00",
+            "16:00",
+            "17:00",
+            "18:00",
+            "19:00",
+            "20:00",
+            "21:00",
+            "22:00",
+            "23:00",
+          ]}
+          options2={[
+            "10:00",
+            "11:00",
+            "12:00",
+            "13:00",
+            "14:00",
+            "15:00",
+            "16:00",
+            "17:00",
+            "18:00",
+            "19:00",
+            "20:00",
+            "21:00",
+            "22:00",
+            "23:00",
+          ]}
+          setValue={setValue}
+        />
 
-    return (
-        <div className="">
-            <Header title={'모집공고'} />
-            <div className="mx-8">
-                {/* 매장이름 */}
-                <InputValue2 label={'매장이름'} setValue={setValue} dict_key={'store_name'} dict_value={recruitData['store_name']} />
-                {/* 매장주소 */}
-                <InputValue2 label={'매장주소'} setValue={setValue} dict_key={'address'} dict_value={recruitData['address']} />
-                {/* 알바유형 */}
-                <InputValue2
-                    mode="SELECT"
-                    label={'알바유형'}
-                    title={'알바유형 선택'}
-                    setValue={setValue}
-                    dict_key={'type'}
-                    dict_value={recruitData['types']}
-                    options={recruitData['types']}
-                />
-                {/* 상세설명 */}
-                <InputValue2
-                    title="해당 공고에 대한 설명 부탁드려요."
-                    label={'상세설명'}
-                    setValue={setValue}
-                    dict_key={'description'}
-                    dict_value={recruitData['description']}
-                />
-            </div>
-            <div className="border-t-4 mt-8 mb-4"></div>
-            {/* --------------------------------------------- */}
+        {/* 시급 설정 */}
+        <InputValue2
+          title="시급을 설정해주세요."
+          label={"시급설정"}
+          setValue={setValue}
+          dict_key={"price"}
+          dict_value={recruitData["price"]}
+        />
+      </div>
 
-            <div className="mx-8">
-                {/* 시작날짜 */}
-                <InputValue2
-                    mode="DATE"
-                    label={'시작날짜'}
-                    title={'시작 날짜 선택'}
-                    setValue={setValue}
-                    dict_key={'start_date'}
-                    dict_value={recruitData['start_date']}
-                />
-                {/* 종료날짜 */}
-                <InputValue2
-                    mode="DATE"
-                    label={'종료날짜'}
-                    title={'종료 날짜 선택'}
-                    setValue={setValue}
-                    dict_key={'end_date'}
-                    dict_value={recruitData['end_date']}
-                />
-                {/* 근무 시간 */}
-                <InputValue2
-                    mode="TIME"
-                    label="근무시간"
-                    options={[
-                        '10:00',
-                        '11:00',
-                        '12:00',
-                        '13:00',
-                        '14:00',
-                        '15:00',
-                        '16:00',
-                        '17:00',
-                        '18:00',
-                        '19:00',
-                        '20:00',
-                        '21:00',
-                        '22:00',
-                        '23:00',
-                    ]}
-                    options2={[
-                        '10:00',
-                        '11:00',
-                        '12:00',
-                        '13:00',
-                        '14:00',
-                        '15:00',
-                        '16:00',
-                        '17:00',
-                        '18:00',
-                        '19:00',
-                        '20:00',
-                        '21:00',
-                        '22:00',
-                        '23:00',
-                    ]}
-                    setValue={setValue}
-                />
-
-                {/* 시급 설정 */}
-                <InputValue2
-                    title="시급을 설정해주세요."
-                    label={'시급설정'}
-                    setValue={setValue}
-                    dict_key={'price'}
-                    dict_value={recruitData['price']}
-                />
-            </div>
-
-            <div className="mx-8">
-                <Button title="모집공고" onClickEvent={onEnroll} />
-            </div>
-        </div>
-    );
+      <div className="mx-8">
+        <Button title="모집공고" onClickEvent={onEnroll} />
+      </div>
+    </div>
+  );
 };
 
 export default OwnerRecruitNoticePage;
