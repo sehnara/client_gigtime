d05ad9bf5db1e366f40d4b3ff51c235687db4307
sehnara <gsh723@naver.com>
2022-08-01 02:42:36 +0900

    temp save

--- src/components/Map/MapRoute.jsx
+++ src/components/Map/MapRoute.jsx
@@ -1,6 +1,34 @@
 import React, { useEffect } from "react";
 const { kakao } = window;
 const MapRoute = () => {
+  const mock = [
+    {
+      store: "수수커피 강남N타워점",
+      lat: "37.4997777",
+      lnt: "127.0324107",
+    },
+    {
+      store: "클로리스 역삼 GFC점",
+      lat: "37.4999269",
+      lnt: "127.0365526",
+    },
+    {
+      store: "바게트케이",
+      lat: "37.5002567",
+      lnt: "127.0414329",
+    },
+    {
+      store: "브루커피",
+      lat: "37.4967293",
+      lnt: "127.0294237",
+    },
+    {
+      store: "커피스니퍼 센터필드점",
+      lat: "37.5030426",
+      lnt: "127.041588",
+    },
+  ];
+
   useEffect(() => {
     const container = document.getElementById("myMap");
     const options = {
@@ -9,16 +37,14 @@ const MapRoute = () => {
     };
     const map = new kakao.maps.Map(container, options);
 
-    var content =
-      '<div class="overlaybox" >' +
-      '    <div class="boxtitle">1</div>' +
-      "</div>";
-
     // 커스텀 오버레이가 표시될 위치입니다
-    for (let i = 0; i < [1, 2, 3, 4, 5].length; i++) {
+    for (let i = 0; i < mock.length; i++) {
       var customOverlay = new kakao.maps.CustomOverlay({
-        position: new kakao.maps.LatLng(37.49887, `12${i}.02658`),
-        content: content,
+        position: new kakao.maps.LatLng(mock[i].lat, mock[i].lnt),
+        content:
+          '<div class="overlaybox" >' +
+          `    <div class="boxtitle">${i + 1}</div>` +
+          "</div>",
         xAnchor: 0.3,
         yAnchor: 0.91,
       });
@@ -30,3 +56,23 @@ const MapRoute = () => {
 };
 
 export default MapRoute;
+
+// 클로리스 역삼 GFC점
+// 서울 강남구 테헤란로 152 강남파이낸스센터 지하1층
+// 37.49992690000000000
+// 127.03655260000000000
+
+// 바게트케이
+// 서울 강남구 테헤란로34길 21-10
+// 37.50025670000000000
+// 127.04143290000000000
+
+// 브루커피 강남타워300점
+// 서울 강남구 테헤란로2길 21 1
+// 37.49672930000000000
+// 127.02942370000000000
+
+// 커피스니퍼 센터필드점
+// 서울 강남구 테헤란로 231 센터필드 지상 2층 E207호 커피스니퍼
+// 37.50304260000000000
+// 127.04158800000000000
